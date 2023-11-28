66d0547221770b90c15e9897ca9802567402f48d
mamasitta <vikiber2013@gmail.com>
2023-11-28 08:31:13 +0100

    updates 28/11

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 
-import {parseCronExpression} from "cron-schedule";
-import {TimerBasedCronScheduler as scheduler} from "cron-schedule/schedulers/timer-based.js";
+//import {parseCronExpression} from "cron-schedule";
+//import {TimerBasedCronScheduler as scheduler} from "cron-schedule/schedulers/timer-based.js";
 
 console.log('Script started successfully');
 
@@ -15,35 +15,44 @@ WA.onInit().then(() => {
     // Julia custom
 
     // At 19:00, turn on night
-    const cronStartNight = parseCronExpression('0 19 * * *');
-    scheduler.setInterval(cronStartNight, () => {
-        WA.room.showLayer("night");
-        WA.room.showLayer("light");
-    });
+    //const cronStartNight = parseCronExpression('0 19 * * *');
+    //scheduler.setInterval(cronStartNight, () => {
+        //WA.room.showLayer("night");
+        //WA.room.showLayer("light");
+    //});
 
     // At 7:00, turn on day
-    const cronStartDay = parseCronExpression('0 7 * * *');
-    scheduler.setInterval(cronStartDay, () => {
-        WA.room.hideLayer("night");
-        WA.room.hideLayer("light");
-    });
+    //const cronStartDay = parseCronExpression('0 7 * * *');
+    //scheduler.setInterval(cronStartDay, () => {
+        //WA.room.hideLayer("night");
+        //WA.room.hideLayer("light");
+    //});
 
     // If the player enters the room between 19:00 and 7:00, turn on night
-    const date = new Date();
-    const startNight = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 19, 0, 0);
-    const startDay = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 7, 0, 0);
-    console.log(startDay)
-    console.log(startNight)
-    console.log(date)
-    console.log(date < startNight)
-    console.log(date > startDay)
-    console.log(date < startNight && date > startDay)
-    if (date < startNight && date > startDay) {
+    //const date = new Date();
+    //const startNight = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 19, 0, 0);
+    //const startDay = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 7, 0, 0);
+    //console.log(startDay)
+    //console.log(startNight)
+    //console.log(date)
+    //console.log(date < startNight)
+    //console.log(date > startDay)
+    //console.log(date < startNight && date > startDay)
+    //if (date < startNight && date > startDay) {
         
-        WA.room.hideLayer("night");
-        WA.room.hideLayer("light");
+        //WA.room.hideLayer("night");
+        //WA.room.hideLayer("light");
         
-    }
+    //}
+
+    // Julia custom
+    WA.room.onEnterLayer("rooms_floor").subscribe(() => {
+        WA.room.hideLayer("facade");
+      });
+      
+    WA.room.onLeaveLayer("rooms_floor").subscribe(() => {
+        WA.room.showLayer("facade");
+      });
 
 
     }).catch(e => console.error(e));
